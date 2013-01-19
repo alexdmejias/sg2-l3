dbedb6ebad6e54f4b88fb792fb1b8a83e13cf11a
alex mejias <amejias101@qc.cuny.edu>
2013-01-19 02:21:53 -0500

    added actualy content to about and pledge

--- application/views/main/about.blade.php
+++ application/views/main/about.blade.php
@@ -1,37 +1,19 @@
 @layout('layouts/main_layout')
 @section('content')
 
-	<div class="container_48">
-		<div class="grid_48">
+	<div  class="container_48">
+		<div  class="grid_48">
 			<div id="content" class="grid_44 push_2"><!--actual content-->
 
 				<div id="main-left" class="grid_20 push_1 alpha"><!--Left main content -->
-					<h1>Our Mission</h1>
-					<p class="mission">Our mission is to provide people with simple way to save money around the house, the office, or any other enviroment they interact with. To read more about who we are visit our <b><a href="about.php">'About us'</a></b> page.
-					<h1 class="quick-tips">Quick Tips</h1>
-					<ul id="preview-list">
-						<li><p  class="">When doing laundry, dry loads consecutively, while the temperature inside of it is still high.</p> </li>
-						<li><p class="">Turn off the water while you brush your teeth. There is never a good reason for waste.</p></li>
-						<li><p class="">When purchasing appliances, consider buying "energystar" certified appliances to radically reduce the amount of electricity used by older appliances.</p></li>
-						<li><p class="">When purchasing appliances or faucets, look for "watersense" certification, it is the equivalent of "energystar" for water usage.</p></li>
-						<li><p class="">Consider replacing reading materials to electronic formats. Ereaders are perfect for paperbacks, documents, and many articles. Their weight and size often times make them extremely more portable than their paper counter-parts.</p></li>
-						<li><p class="">Instead of taking a bath, try taking a shower instead. A short shower can drastically reduce the amount of water used, it also reduces the amount of energy requiered to heat all the water used.</p></li>
-					</ul>
-					<p><b><a href="tips.php">View more tips</a></b></p>
+					<h1>Who are we?</h1>
+					<p>We are a team of students headed by <a href="http://alex-mejias.com">Alex Mejias</a> from Queens Collge, who has realized that for a variety of reasons, people are not concious of the enviroment and their impact on it. The team behind Greenserve has realized that in order to persuade people that there are benefits to going "green", we have crated Greenserve.</p>
+
 				</div> <!--end left main content -->
 
 				<div id="main-right" class="grid_21 push_3 omega">
-				<!--draw graphs from 'charts.js'-->
-					<div class="figure">
-						<h1>CFL Lightbulb Savings</h1>
-						<div id="graph_cfl"></div>
-					</div>
-
-					<div class="hide">
-						<h1>Water Usage per Minute</h1>
-						<div id="graph_gpm"></div>
-					</div>
-
+					<h1>Why?</h1>
+					<p>The main reason why we have spent so much time and effort into building this website is so people can visually see how some of the actions they can take can save them a lot of money. At Greenserve we realized that just by telling people to go "green" will not help or do anything. In order to motivate people to take more consious decisions we will try to show them that they can truly save money, and the more 'tips' you follow, the more money you will be able to see.</p>
 				</div>
 			</div>
 		</div>
--- application/views/main/pledge.blade.php
+++ application/views/main/pledge.blade.php
@@ -2,39 +2,68 @@
 @section('content')
 
 	<div class="container_48">
-		<div class="grid_48">
+		<div  class="grid_48">
 			<div id="content" class="grid_44 push_2"><!--actual content-->
-
-				<div id="main-left" class="grid_20 push_1 alpha"><!--Left main content -->
-					<h1>Our Mission</h1>
-					<p class="mission">Our mission is to provide people with simple way to save money around the house, the office, or any other enviroment they interact with. To read more about who we are visit our <b><a href="about.php">'About us'</a></b> page.
-					<h1 class="quick-tips">Quick Tips</h1>
-					<ul id="preview-list">
-						<li><p  class="">When doing laundry, dry loads consecutively, while the temperature inside of it is still high.</p> </li>
-						<li><p class="">Turn off the water while you brush your teeth. There is never a good reason for waste.</p></li>
-						<li><p class="">When purchasing appliances, consider buying "energystar" certified appliances to radically reduce the amount of electricity used by older appliances.</p></li>
-						<li><p class="">When purchasing appliances or faucets, look for "watersense" certification, it is the equivalent of "energystar" for water usage.</p></li>
-						<li><p class="">Consider replacing reading materials to electronic formats. Ereaders are perfect for paperbacks, documents, and many articles. Their weight and size often times make them extremely more portable than their paper counter-parts.</p></li>
-						<li><p class="">Instead of taking a bath, try taking a shower instead. A short shower can drastically reduce the amount of water used, it also reduces the amount of energy requiered to heat all the water used.</p></li>
-					</ul>
-					<p><b><a href="tips.php">View more tips</a></b></p>
-				</div> <!--end left main content -->
-
-				<div id="main-right" class="grid_21 push_3 omega">
-				<!--draw graphs from 'charts.js'-->
-					<div class="figure">
-						<h1>CFL Lightbulb Savings</h1>
-						<div id="graph_cfl"></div>
+				<div id="content-top">
+					<div id="main-left" class="grid_20 push_1 alpha"><!--Left main content -->
+						<blockquote>"From now on, I pledge to conserve paper,water, and electricity in order save both money and the planet."</blockquote>
+					</div><!--end left main content -->
+					<div id="main-right" class="grid_20 push_3 omega">
+						<h2 id="main_sign">Your email adress</h2>
+						<form method="post" action="php/pledge_process.php">
+								<input type="text" id="main_sign_email_field" name="main_sign_email_field"/>
+								<input type="submit" value="Sign the pledge!" name="submit" id="main_sign_button"/>
+						</form>
 					</div>
-
-					<div class="hide">
-						<h1>Water Usage per Minute</h1>
-						<div id="graph_gpm"></div>
+				</div><!--  END content-top -->
+				<div id="content-bottom">
+					<div class="grid_22 prefix_1 alpha hide">
+						<script src="http://widgets.twimg.com/j/2/widget.js"></script>
+						<script>
+							new TWTR.Widget({
+								version: 2,
+								type: 'search',
+								search: '#grnsrve',
+								interval: 6000,
+								title: '',
+								subject: 'save green by saving green',
+								width: 400,
+								height: 215,
+								theme: {
+									shell: {
+										background: '#8ec1da',
+										color: '#ffffff'
+									},
+									tweets: {
+										background: '#ffffff',
+										color: '#444444',
+										links: '#1985b5'
+									}
+								},
+								features: {
+									scrollbar: false,
+									loop: true,
+									live: true,
+									hashtags: true,
+									timestamp: true,
+									avatars: true,
+									toptweets: true,
+									behavior: 'default'
+								}
+							}).render().start();
+						</script>
 					</div>
-
+					<div id="fb" class="grid_20 omega hide ">
+						<div id="fb-root">
+						</div>
+						<script src="http://connect.facebook.net/en_US/all.js#xfbml=1"></script>
+						<fb:like-box href="http://www.facebook.com/pages/Greenserve/158651644197000" width="400" show_faces="true" stream="false" header="true">
+						</fb:like-box>
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
+	{{--</div><!-- end container 48  --> --}}
 
 @endsection
