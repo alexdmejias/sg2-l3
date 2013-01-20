761c51cfae6b10451a0dd9f6aef25ebf1880b46e
alex mejias <amejias101@qc.cuny.edu>
2013-01-19 22:13:15 -0500

    moved slider to its own element more content

--- application/views/main/about.blade.php
+++ application/views/main/about.blade.php
@@ -8,8 +8,7 @@
 				<div id="main-left" class="grid_20 push_1 alpha"><!--Left main content -->
 					<h1>Who are we?</h1>
 					<p>We are a team of students headed by <a href="http://alex-mejias.com">Alex Mejias</a> from Queens Collge, who has realized that for a variety of reasons, people are not concious of the enviroment and their impact on it. The team behind Greenserve has realized that in order to persuade people that there are benefits to going "green", we have crated Greenserve.</p>
-
-				</div> <!--end left main content -->
+				</div>
 
 				<div id="main-right" class="grid_21 push_3 omega">
 					<h1>Why?</h1>
@@ -18,5 +17,4 @@
 			</div>
 		</div>
 	</div>
-
 @endsection
--- application/views/main/why.blade.php
+++ application/views/main/why.blade.php
@@ -4,37 +4,39 @@
 	<div class="container_48">
 		<div class="grid_48">
 			<div id="content" class="grid_44 push_2"><!--actual content-->
-
 				<div id="main-left" class="grid_20 push_1 alpha"><!--Left main content -->
-					<h1>Our Mission</h1>
-					<p class="mission">Our mission is to provide people with simple way to save money around the house, the office, or any other enviroment they interact with. To read more about who we are visit our <b><a href="about.php">'About us'</a></b> page.
-					<h1 class="quick-tips">Quick Tips</h1>
-					<ul id="preview-list">
-						<li><p  class="">When doing laundry, dry loads consecutively, while the temperature inside of it is still high.</p> </li>
-						<li><p class="">Turn off the water while you brush your teeth. There is never a good reason for waste.</p></li>
-						<li><p class="">When purchasing appliances, consider buying "energystar" certified appliances to radically reduce the amount of electricity used by older appliances.</p></li>
-						<li><p class="">When purchasing appliances or faucets, look for "watersense" certification, it is the equivalent of "energystar" for water usage.</p></li>
-						<li><p class="">Consider replacing reading materials to electronic formats. Ereaders are perfect for paperbacks, documents, and many articles. Their weight and size often times make them extremely more portable than their paper counter-parts.</p></li>
-						<li><p class="">Instead of taking a bath, try taking a shower instead. A short shower can drastically reduce the amount of water used, it also reduces the amount of energy requiered to heat all the water used.</p></li>
+					<h1>Why save?</h1>
+					<p class="white">Energy saving is very important to all of us for many different reasons. The aim of "greenserving" is to provide no-cost, low-cost and sometimes even projects to help you save water, electricity, or paper, whether youare mainly doing it for the money or the enviroment. </p>
+					<br /><br />
+					<p class="white">When it comes to conserving resources, the tips in this website are divided into three categories of cost and effort.</p>
+					<ul class="info_list">
+						<li><p class="white"><b>No-Cost</b> tips involve slight behavioral changes from the user, tips like these involve turning off lights, and closing doors.</p></li>
+						<li><p class="white"><b>Low-cost</b> tips involve buying products that conserve and have a relatevely low price range. </p></li>
+						<li><p class="white"><b>Investments</b> are the most expensive tips on the site, but they are the biggest conservers on resources in the long run.</p></li>
 					</ul>
-					<p><b><a href="tips.php">View more tips</a></b></p>
-				</div> <!--end left main content -->
-
-				<div id="main-right" class="grid_21 push_3 omega">
-				<!--draw graphs from 'charts.js'-->
-					<div class="figure">
-						<h1>CFL Lightbulb Savings</h1>
-						<div id="graph_cfl"></div>
-					</div>
-
-					<div class="hide">
-						<h1>Water Usage per Minute</h1>
-						<div id="graph_gpm"></div>
-					</div>
-
 				</div>
+				<div id="main-right" class="grid_21 push_3 omega">
+					<h1>&nbsp;</h1>
+						<p class="white">These are some of the reasons why you might want to think about conserving:</p>
+						<ul class="info_list">
+							<li><p class="white">Depending on your actions, you can save a lot of money each year.</p></li>
+							<li><p class="white">It makes for a more appealing home to sell if you ever try to sell it.</p></li>
+							<li><p class="white">The price of utilities will keeping increasing as time pases by.</p></li>
+							<li><p class="white">You will feel a lot better once you know you are saving money and the right thing.</p></li>
+						</ul>
+				</div> <!--end left main content -->
+			</div>
+			<div id="main-lower-left" class="grid_14 push_2 alpha">
+				<h2></h2>
+			</div>
+			<div id="main-lower-middle" class="grid_14  push_3 ">
+				<h2></h2>
+			</div>
+			<div id="main-lower-right" class="grid_14  push_4 omega">
+				<h2></h2>
 			</div>
 		</div>
 	</div>
+
 
 @endsection
